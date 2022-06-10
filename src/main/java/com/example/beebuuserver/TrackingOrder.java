b1c5031336aec89f992d1b297b82c55f4d3c36ff
andersonloi1998 <43953992+andersonloi1998@users.noreply.github.com>
2022-06-10 08:30:52 +0800

    Add files via upload

---
+++ src/main/java/com/example/beebuuserver/TrackingOrder.java
@@ -0,0 +1,83 @@
+package com.example.beebuuserver;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import info.hoang8f.widget.FButton;
+
+public class TrackingOrder extends Activity {
+
+    private GoogleMap mMap;
+    Button btnDrive,btnBicycle,btnWalk;
+    String uriString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_order_status);
+
+        btnDrive = findViewById(R.id.btnDrive);
+
+        btnDrive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uriString = String.format("google.navigation:q=24.1826712,120.6445135&mode=d");
+                launchMap();
+            }
+        });
+
+        btnBicycle = findViewById(R.id.btnBicycle);
+
+        btnBicycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uriString = String.format("google.navigation:q=24.1826712,120.6445135&mode=b");
+                launchMap();
+            }
+        });
+
+        btnWalk = findViewById(R.id.btnWalk);
+
+    }
+
+    private void launchMap() {
+        Uri intentUri = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW,intentUri);
+        intent.setPackage("com.google.android.apps.maps");
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+    }
+
+}
